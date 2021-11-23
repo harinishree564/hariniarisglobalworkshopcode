18f9bff868c38edba4c591d86d0e88d1c7487dfe
harinishree564 <94341770+harinishree564@users.noreply.github.com>
2021-11-23 21:17:52 +0530

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,16 @@
+interface person1 { 
+    firstName:String;
+    lastName: String;
+    salary:number;
+}
+interface person2 {
+    firstName:String;
+    lastName: String;
+    salary:number;
+}
+let customer: person2 = {
+    firstName = "harini",
+    lastName  = "m s",
+    salary = 1000
+};
+console.log(customer);
