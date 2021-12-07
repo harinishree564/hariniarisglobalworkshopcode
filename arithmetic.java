0f5a63595fb0f5f75cc4a9782305a8ab21700f30
harinishree564 <94341770+harinishree564@users.noreply.github.com>
2021-12-07 14:43:12 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+package project2;
+
+public class arithmetic {
+	
+	    public static void main(String[] args)
+	    {
+	        int ar[] = { 1, 2, 3, 4, 5 };
+	        for (int i = 0; i <= ar.length; i++)
+	            System.out.println(ar[i]);
+	    }
+	}
+
